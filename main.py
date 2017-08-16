a5afb98d149cbfc71f514be0431a85e523e61432
Steven Yeu <yeusteven@gmail.com>
2017-08-16 14:49:36 -0700

    update main

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def homepage():
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
 
-    <img src="http://loremflickr.com/600/400">
+    <img src="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjP04zj3dzVAhUP7WMKHUUlBi0QjBwIBA&url=http%3A%2F%2Fcdn2-www.dogtime.com%2Fassets%2Fuploads%2Fgallery%2Fcorgi-puppies%2Fcorgi-puppy-3.jpg&psig=AFQjCNHCSuZPOydvXHIku2dfGY45hGwNQQ&ust=1503006527000697">
     """.format(time=the_time)
 
 if __name__ == '__main__':
