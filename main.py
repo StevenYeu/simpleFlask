4aa9cde125cdd4045e225b49fbe3527874e8c4f3
Steven Yeu <yeusteven@gmail.com>
2017-08-16 14:44:20 -0700

    update pfile again

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask
-
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'This is the homepage'
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
